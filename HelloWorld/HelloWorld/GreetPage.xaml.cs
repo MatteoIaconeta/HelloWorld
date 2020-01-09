2b8bd24aab3a12ff803a2f8335f22a5d437d84d7
Matteo Iaconeta <matteo.iaconeta@microarea.it>
2020-01-09 15:37:34 +0100

    16. Dealing with Device Differences
    Il metodo OnPlatform usato nel video è obsoleto, usato uno switch con la proprietà RuntimePlatform per avere un comportamento similare

--- HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -15,6 +15,21 @@ namespace HelloWorld
         public GreetPage()
         {
             InitializeComponent();
+
+            slider.Value = 0.5;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+                case Device.Android:
+                    Padding = new Thickness(10, 20, 0, 0);
+                    break;
+                case Device.UWP:
+                    Padding = new Thickness(30, 20, 0, 0);
+                    break;
+            }
         }
     }
 }
