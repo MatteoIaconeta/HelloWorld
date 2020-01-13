158f77c6cf9865332b9ea15e93a1094ffe9025a7
Matteo Iaconeta <matteo.iaconeta@microarea.it>
2020-01-13 12:51:27 +0100

    29. Absolute Layout in Code
    Componente Absolute Layout in C# con esempi d'uso

--- HelloWorld/HelloWorld/AbsolutePage.xaml.cs
+++ HelloWorld/HelloWorld/AbsolutePage.xaml.cs
@@ -15,6 +15,14 @@ namespace HelloWorld
 		public AbsolutePage ()
 		{
 			InitializeComponent ();
-		}
+
+            var layout = new AbsoluteLayout();
+
+            layout.Children.Add(new BoxView { Color = Color.Aqua }, new Rectangle(0, 0, 1, 1), AbsoluteLayoutFlags.All);
+            layout.Children.Add(new BoxView { Color = Color.White }, new Rectangle(0.5, 0.1, 100, 100), AbsoluteLayoutFlags.PositionProportional);
+            layout.Children.Add(new Button { Text = "Get Started", BackgroundColor = Color.Silver, TextColor = Color.White }, new Rectangle(0, 1, 1, 50), AbsoluteLayoutFlags.PositionProportional | AbsoluteLayoutFlags.WidthProportional);
+
+            Content = layout;
+        }
 	}
 }
