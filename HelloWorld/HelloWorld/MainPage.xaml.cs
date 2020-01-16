55130cd5374efbf21937f17db4cad30a64e8784e
Matteo Iaconeta <matteo.iaconeta@microarea.it>
2020-01-16 15:00:11 +0100

    65. Displaying Popups
    Esempi d'utilizzo di popup

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -9,11 +9,21 @@ using Xamarin.Forms.Xaml;
 namespace HelloWorld
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : CarouselPage
+    public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();            
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            //var response = await DisplayAlert("Warning", "Are you sure?", "Yes", "No");
+            //if (response)
+            //    await DisplayAlert("Done", "Your response will be saved!", "OK");
+
+            var response = await DisplayActionSheet("Title", "Cancel", "Delete", "Copy Link", "Message", "Email");
+            await DisplayAlert("Response", response, "OK");
         }
     }
 }
