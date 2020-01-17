bded0c2fa11a4050fa7624591b22df491740a9db
Matteo Iaconeta <matteo.iaconeta@microarea.it>
2020-01-17 12:33:26 +0100

    74. Picker
    Componente Picker con esempi d'uso

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -9,21 +9,40 @@ using Xamarin.Forms.Xaml;
 namespace HelloWorld
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public class ContactMethod
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
     public partial class MainPage : ContentPage
     {
+        private IList<ContactMethod> contactMethods;
         public MainPage()
         {
             InitializeComponent();
+
+            contactMethods = GetContactMethods();
+
+            foreach (var method in contactMethods)
+                picker.Items.Add(method.Name);
         }
 
-        //private void Entry_Completed(object sender, EventArgs e)
-        //{
-        //    label.Text = "Completed";
-        //}
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var name = picker.Items[picker.SelectedIndex];
+            var contactMethod = contactMethods.Single(cm => cm.Name.Equals(name));
 
-        //private void Entry_TextChanged(object sender, TextChangedEventArgs e)
-        //{
-        //    label.Text = e.NewTextValue;
-        //}
+            DisplayAlert("Selection", name, "OK");
+        }
+
+        private IList<ContactMethod> GetContactMethods()
+        {
+            return new List<ContactMethod>
+            {
+                new ContactMethod { Id = 1, Name = "SMS" },
+                new ContactMethod { Id = 2, Name = "Email" }
+            };
+        }
     }
 }
