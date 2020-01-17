e85048b4fac0670623153abe6e3697920f540b71
Matteo Iaconeta <matteo.iaconeta@microarea.it>
2020-01-17 12:55:39 +0100

    75. Date and Time Picker
    Componenti DatePicker & TimePicker con esempi d'uso

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -9,40 +9,16 @@ using Xamarin.Forms.Xaml;
 namespace HelloWorld
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-
-    public class ContactMethod
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
     public partial class MainPage : ContentPage
     {
-        private IList<ContactMethod> contactMethods;
         public MainPage()
         {
             InitializeComponent();
-
-            contactMethods = GetContactMethods();
-
-            foreach (var method in contactMethods)
-                picker.Items.Add(method.Name);
         }
 
-        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
         {
-            var name = picker.Items[picker.SelectedIndex];
-            var contactMethod = contactMethods.Single(cm => cm.Name.Equals(name));
-
-            DisplayAlert("Selection", name, "OK");
-        }
-
-        private IList<ContactMethod> GetContactMethods()
-        {
-            return new List<ContactMethod>
-            {
-                new ContactMethod { Id = 1, Name = "SMS" },
-                new ContactMethod { Id = 2, Name = "Email" }
-            };
+            //e.NewDate & e.OldDate
         }
     }
 }
