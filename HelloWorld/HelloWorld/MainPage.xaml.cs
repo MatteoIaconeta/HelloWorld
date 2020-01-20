4604e0ff4f518b0c0f04e6f914f35108ecfc55bf
Matteo Iaconeta <matteo.iaconeta@microarea.it>
2020-01-20 14:40:53 +0100

    85. A Cleaner Implementation
    Migliorato il codice dell'esempio precedente sfruttando il meccanismo di DataBinding

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -16,23 +16,12 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            if (Application.Current.Properties.ContainsKey("Name"))
-                title.Text = Application.Current.Properties["Name"].ToString();
-            if (Application.Current.Properties.ContainsKey("NotificationsEnabled"))
-                notificationsEnabled.On = (bool)Application.Current.Properties["NotificationsEnabled"];
+            BindingContext = Application.Current;
         }
 
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
-        }
-
-        private void OnChange(object sender, EventArgs e)
-        {
-            Application.Current.Properties["Name"] = title.Text;
-            Application.Current.Properties["NotificationsEnabled"] = notificationsEnabled.On;
-
-            //Application.Current.SavePropertiesAsync();
         }
     }
 }
