aff34ee714215bbdf267e9beb4f9eb01c89b1fff
Matteo Iaconeta <matteo.iaconeta@microarea.it>
2020-01-20 16:32:51 +0100

    88. Setting Up SQLite
    Configurazione di SQLite

---
+++ HelloWorld/HelloWorld.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,19 @@
+﻿using HelloWorld.Droid.Persistence;
+using HelloWorld.Persistence;
+using SQLite;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+namespace HelloWorld.Droid.Persistence
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
