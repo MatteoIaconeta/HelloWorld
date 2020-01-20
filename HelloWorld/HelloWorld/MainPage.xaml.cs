97100932b1a5e339c980e3dfd684ad6e9e520293
Matteo Iaconeta <matteo.iaconeta@microarea.it>
2020-01-20 17:23:26 +0100

    89. Using SQLite
    Primi esempi di uso di SQLite

--- HelloWorld/HelloWorld/MainPage.xaml.cs
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using HelloWorld.Extensions;
+using HelloWorld.Persistence;
+using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +13,54 @@ using Xamarin.Forms.Xaml;
 namespace HelloWorld
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public class Recipe
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Name { get; set; }
+    }
+
     public partial class MainPage : ContentPage
     {
+        private SQLiteAsyncConnection connection;
+        private ObservableCollection<Recipe> recipes;
         public MainPage()
         {
             InitializeComponent();
 
-            var fileSystem = DependencyService.Get<IFileSystem>();            
+            connection = DependencyService.Get<ISQLiteDb>().GetConnection();
+        }
+
+        protected override async void OnAppearing()
+        {
+            await connection.CreateTableAsync<Recipe>();
+            recipes = new ObservableCollection<Recipe>(await connection.Table<Recipe>().ToListAsync());
+            recipesListView.ItemsSource = recipes;
+
+            base.OnAppearing();
+        }
+
+        private async void OnAdd(object sender, EventArgs e)
+        {
+            var recipe = new Recipe { Name = "Recipe " + DateTime.Now.Ticks };
+            await connection.InsertAsync(recipe);
+            recipes.Add(recipe);
+        }
+
+        private async void OnUpdate(object sender, EventArgs e)
+        {
+            var recipe = recipes[0];
+            recipe.Name += " UPDATED";
+            await connection.UpdateAsync(recipe);
+        }
+
+        private async void OnDelete(object sender, EventArgs e)
+        {
+            var recipe = recipes[0];
+            await connection.DeleteAsync(recipe);
+            recipes.Remove(recipe);
         }
     }
 }
